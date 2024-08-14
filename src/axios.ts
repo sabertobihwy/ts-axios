242f213eb947f15bb264fcd3fc7e367b0c99e163
wenyan <sabertobihwy@gmail.com>
2024-08-13 22:35:03 -0400

    const instance = axios.create({config}) instance({config})

--- src/axios.ts
+++ src/axios.ts
@@ -1,14 +1,19 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './defaults'
+import mergeConfig from './core/MergeStrat'
 
-export function createInstance(initConfig:AxiosRequestConfig):AxiosInstance{
+export function createInstance(initConfig:AxiosRequestConfig):AxiosStatic{
     const context = new Axios(initConfig)
     const instance = Axios.prototype.request.bind(context)
     extend(instance,context)
-    return instance as AxiosInstance
+    return instance as AxiosStatic
+}
+const axios = createInstance(defaults)
+
+axios.create = function(customConfig:AxiosRequestConfig){
+  return createInstance(mergeConfig(defaults,customConfig))
 }
 
-const axios = createInstance(defaults)
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -79,3 +79,7 @@ export interface AxiosInstance extends Axios{
   <T=any>(config:AxiosRequestConfig):AxiosPromise<T>
   <T=any>(url:string, config?:AxiosRequestConfig):AxiosPromise<T>
 }
+
+export interface AxiosStatic extends AxiosInstance{
+  create(config:AxiosRequestConfig): AxiosStatic
+}
